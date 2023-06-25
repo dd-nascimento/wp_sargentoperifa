a87874525f7a301a257b88453d0c279275f6fbbd
David Nascimento <dnmnascimento@outlook.com>
2023-06-25 17:56:04 -0300

    Retirando links de CSS da página para inclusão via função PHP do WordPress.

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal Sargento Perifa</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
