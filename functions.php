b1a3ed5446e298b71c24783e21f62304eec7ae8b
David Nascimento <dnmnascimento@outlook.com>
2023-06-25 18:28:31 -0300

    Concluíndo mudanças do Bootstrap para functions e retirada do link JS do footer para as funções php.

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
 
 function load_scripts(){
+
+    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.0', true);
+
+    wp_enqueue_style('bootstra-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css', array(), '5.3.0', 'all');
+
     wp_enqueue_style('template', get_template_directory_uri(). '/_css/template.css', array(), '1.0', 'all');
 }
 
